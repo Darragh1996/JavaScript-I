8ee62bcd5416b7dd7813388dcd0f2e3b44370cb5
Darragh Ferry <darraghferry@hotmail.com>
2019-10-14 13:37:21 +0100

    changed solution for challenge 6 in arrays.js to use filter method

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -117,12 +117,17 @@ console.log(oldCars.length);
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
-let BMWAndAudi = [];
-for (i=0; i<inventory.length; i++){
-  if (inventory[i].car_make == 'Audi' || inventory[i].car_make == 'BMW' ){
-    BMWAndAudi.push(inventory[i]);
+// let BMWAndAudi = [];
+let BMWAndAudi = inventory.filter(function (carObj){
+  if (carObj.car_make == 'Audi' || carObj.car_make == 'BMW' ){
+    return carObj;
   }
-}
+});
+// for (i=0; i<inventory.length; i++){
+//   if (inventory[i].car_make == 'Audi' || inventory[i].car_make == 'BMW' ){
+//     BMWAndAudi.push(inventory[i]);
+//   }
+// }
 
 for (i=0; i<BMWAndAudi.length; i++){
   console.log(JSON.stringify(BMWAndAudi[i]));
