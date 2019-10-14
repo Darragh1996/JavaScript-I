b2e7faf376118d9323bec537892aea9a8b7b7809
Darragh Ferry <darraghferry@hotmail.com>
2019-10-14 13:43:13 +0100

    changed solution for challenge 5 in arrays.js to use pointer

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -109,9 +109,7 @@ console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-let oldCars = inventory.filter(function (carObj) {
-  return carObj.car_year < 2000;
-})
+let oldCars = inventory.filter((carObj) => carObj.car_year < 2000)
 
 console.log(oldCars.length);
 
